6df32de0218ec9a4eaef37244918e56c5558c50b
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-11-18 18:47:18 -0300

    migration, resources

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['category_id', 'title', 'slug','content', 'is_published'];
+
+    //One to Many reverse
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Many to Many Tags
+    public function Tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+    
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    // Many to Many
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
